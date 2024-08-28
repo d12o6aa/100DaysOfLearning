70c9be5838658171619c408fe6b4d19f0e195e7d
d12o6aa <doaakarem798@gmail.com>
2024-08-28 09:38:37 +0300

    task day 4

---
+++ Python/003/004/script.py
@@ -0,0 +1,25 @@
+
+# 1. Write a program that declares two integer variables and perform basic arithmetic operations (addition, subtraction, multiplication, division) on them. Print the results to the console.
+x = int(input())
+y = int(input())
+print("addition : ")
+print(x+y)
+print("subtraction : ")
+print(x-y)
+print("multiplication : ")
+print(x*y)
+print("division : ")
+print(x/y)
+
+# 2. Write a program that calculates the area of a rectangle. Prompt the user to input the length(integer) and width(integer) of the rectangle, calculate the area (length * width), and print the result.
+
+w = int(input("enter the width"))
+h = int(input("enter the height"))
+print("the area of rectangle is : ")
+print(w*h)
+
+# 3. Modify the above program to read decimal numbers as the length and width, and output the area to two decimal points
+ww = float(input("enter the width"))
+hh = float(input("enter the height"))
+print("the area of rectangle is : ")
+print(w*h)
