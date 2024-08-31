5d6e95498e10b31fffa669ed21ff5d53984a849f
d12o6aa <doaakarem798@gmail.com>
2024-09-01 00:51:55 +0300

    problem 1

---
+++ Python/005/problem1.py
@@ -0,0 +1,7 @@
+#Write a program that reads an integer as user input and prints whether the number is Odd or Even to the console
+
+num = int(input("enert a number"))
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
