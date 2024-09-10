463128181771a7c3936468e5c0e0d568e40b54f4
d12o6aa <doaakarem798@gmail.com>
2024-09-10 16:35:17 +0300

    8

---
+++ Python/008/README.md
@@ -0,0 +1,5 @@
+
+# Day 8 from Challenge Of Python
+
+## Create a simple calculator program that can add, subtract, multiply, and divide two integers
+
---
+++ Python/008/day8.py
@@ -0,0 +1,13 @@
+
+c = input("enter your choice + | - | * | / : ")
+x = int(input("the first number : "))
+y = int(input("the second number : "))
+if c == "+":
+    print(x+y)
+elif c == "*":
+    print(x*y)
+elif c == "-":
+    print(x-y)
+elif c == "/":
+    print(x/y)
+
