2c87abd811abc19ff88e886643e0c8f6f161658f
d12o6aa <doaakarem798@gmail.com>
2024-09-10 16:27:40 +0300

    day 7

---
+++ Python/007/day7.py
@@ -0,0 +1,9 @@
+Celsius = int(input("Enter Celsius: "))
+CtF =  Celsius * 1.8 + 32
+print(int(CtF))
+
+#############################
+
+Fahrenheit = int (input("Enter Fahrenheit: "))
+FtC = (Fahrenheit - 32 )/1.8
+print(int(FtC))
